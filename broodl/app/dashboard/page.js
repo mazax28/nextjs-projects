20696088cda0c75d508bea394e1b5fbc1f403274
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-03-01 11:07:13 -0300

    initialize Next.js project with Tailwind CSS and basic layout

---
+++ broodl/app/dashboard/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: "Broodl-Dashboard",
+    description: "Dashboard page for Broodl",
+  };
+
+export default function DashboardPage(){
+    return(
+        <div>
+            <h1>Dashboard Page</h1>
+        </div>
+    )
+}
