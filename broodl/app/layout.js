d0ccf495dc64fcc40b379dea65323ec98af305ef
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-03-03 16:07:18 -0300

    replace Geist font with Open Sans and add Header, Footer, and AuthProvider components to layout

--- broodl/app/layout.js
+++ broodl/app/layout.js
@@ -1,16 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Open_Sans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { AuthProvider } from "@/context/AuthContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+const openSans = Open_Sans({ subsets: ["latin"], weight: ["400"] });
 export const metadata = {
   title: "Broodl",
   description: "App for broodl",
@@ -19,11 +14,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <AuthProvider>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col ${openSans.className}  antialiased`}
       >
+        {<Header  />}
         {children}
+        {<Footer  />}
       </body>
+      </AuthProvider>
     </html>
   );
 }
