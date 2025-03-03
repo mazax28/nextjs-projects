971baf271bc8df5b3e9ec4e7d6e61a60e659ce6c
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-03-03 16:07:47 -0300

    add Footer component with creation message

---
+++ broodl/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='p-4 sm:p-8 flex items-center justify-center '>
+        <p className={'text-indigo-600 font-semibold text-xl'}>Created with </p>
+    </div>
+  )
+}
+
+export default Footer
